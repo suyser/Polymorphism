06fa5035bf83ba30612b2311e5a94c2aa24116ab
Кристина <@DESKTOP-UCF1446>
2024-09-15 22:38:12 +0700

    Добавьте файлы проекта.

---
+++ Polymorphism/CustomConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class CustomConverter()
+    {
+        public void Convert(string input, out int result)
+        {
+            result = System.Convert.ToInt32(input);
+        }
+        public void Convert(string input, out string result)
+        {
+            result = System.Convert.ToString(input);
+        }
+        public void Convert(string input, out double result)
+        {
+            result = System.Convert.ToDouble(input);
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using Polymorphism;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CustomConverter converter = new CustomConverter();
+        string input = "123";
+
+        converter.Convert(input, out int result1);
+        Console.WriteLine(result1);
+        converter.Convert(input, out string result2);
+        Console.WriteLine(result2);
+        converter.Convert(input, out double result3);
+        Console.WriteLine(result3);
+    }
+
+}
